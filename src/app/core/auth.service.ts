56e68f6b497f1a898e928a1d23b69f4e8a0ff7e6
Reshma Shaik <kshaikreshma98@gmail.com>
2024-06-16 19:05:33 +0100

    feat: login form

--- src/app/core/auth.service.ts
+++ src/app/core/auth.service.ts
@@ -1,16 +1,26 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
-  private apiUrl = 'http://localhost:8000/api/hello'; // Replace with your backend URL
+  private apiUrl = 'http://localhost:8000/api/v1';
 
   constructor(private http: HttpClient) { }
 
   getHelloMessage(): Observable<{ message: string }> {
-    return this.http.get<{ message: string }>(this.apiUrl);
+    return this.http.get<{ message: string }>(`http://localhost:8000/api/hello`);
+  }
+
+  login(credentials: { email: string; password: string }): Observable<any> {
+
+    const headers = new HttpHeaders({
+      'Content-Type': 'application/json'
+      // Add other headers here if necessary
+    });
+
+    return this.http.post<any>(`${this.apiUrl}/login`, credentials,  { headers });
   }
 }
--- src/app/features/landing-page/right-side/right-side.component.ts
+++ src/app/features/landing-page/right-side/right-side.component.ts
@@ -1,12 +1,44 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { HttpClient } from '@angular/common/http';
+import { Router } from '@angular/router';
+import { AuthService } from '../../../core/auth.service';
 
 @Component({
   selector: 'app-right-side',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, ReactiveFormsModule],
   templateUrl: './right-side.component.html',
-  styleUrl: './right-side.component.css'
+  styleUrls: ['./right-side.component.css']
 })
 export class RightSideComponent {
+  loginForm: FormGroup;
 
+  constructor(
+    private fb: FormBuilder,
+    private http: HttpClient,
+    private router: Router,
+    private authService: AuthService
+  ) {
+    this.loginForm = this.fb.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', Validators.required]
+    });
+  }
+
+  onSubmit(): void {
+    if (this.loginForm.valid) {
+      this.authService.login(this.loginForm.value)
+      .subscribe({
+        next: (response: any) => {
+          console.log(response);
+          this.router.navigate(['/dashboard']);
+        },
+        error: (error: any) => {
+          console.error(error);
+        }
+      });
+    }
+  }
 }
